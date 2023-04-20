5fc754797c4dfaef82e3d091486ebeadbec70258
adibahsatriani <125952703+adibahsatriani@users.noreply.github.com>
2023-04-20 21:26:44 +0700

    Add files via upload

---
+++ 5.5request .php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <a href="http://localhost/Praktek-6/5.4getAct.php?nama=Adibah+Dewi+Satriani&email=dibah.satriani%40gmail.com&btnLogin=Login">Link ke Cek</a>
+           
+    </body>
+</html>
---
+++ 8.1redirect halaman.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="2; url=http://localhost/Praktek-6/">
+    </head>
+    <body>
+        Test Redirect
+    </body>
+</html>
